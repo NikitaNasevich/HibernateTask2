1f4c6c38002fdbebc9e180061beb8da31e0c5aa9
Nikita Nasevich <nasevich.nik@mail.ru>
2023-01-16 16:53:34 +0300

    all task done

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,11 @@
 package org.example;
 
+import org.example.model.Principal;
+import org.example.model.School;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Configuration configuration = new Configuration().addAnnotatedClass(Principal.class)
+                .addAnnotatedClass(School.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            School school = new School(1221);
+            Principal principal = session.get(Principal.class, 5);
+            principal.setSchool(school);
+
+            session.save(school);
+            session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
+        }
+
+
     }
 }
